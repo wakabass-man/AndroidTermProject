2f45d76c6afccd5d9c1d08eb4d48cb09ad355fb1
조준현 <64850905+wakabass-man@users.noreply.github.com>
2021-04-11 23:42:04 +0900

    텀 프로젝트에 리소스 추가
    .

---
+++ Projects/TermProject/app/src/main/java/kr/ac/kpu/s2015184027/termproject/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.kpu.s2015184027.termproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
